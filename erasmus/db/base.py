20baa1421ee5575cc25cfa24d165d3cdfa1c9fd7
Bryan Forbes <bryan@reigndropsfall.net>
2020-04-04 19:28:16 -0500

    Bump deps and use class syntax for base model class

--- erasmus/db/base.py
+++ erasmus/db/base.py
@@ -4,9 +4,11 @@ from botus_receptus.gino import Gino, ModelMixin
 db = Gino()
 
 if TYPE_CHECKING:
-    from gino.declarative import declarative_base
     from gino.crud import CRUDModel
 
-    Base = declarative_base(db, (CRUDModel, ModelMixin))
+    class Base(CRUDModel, ModelMixin):
+        pass
+
+
 else:
     Base = db.Model
