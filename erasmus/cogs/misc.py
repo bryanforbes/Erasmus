77163d5a280696bcc353169fdae276e093375b33
Bryan Forbes <bryan@reigndropsfall.net>
2022-04-01 11:21:40 -0500

    Improve misc commands

--- erasmus/cogs/misc.py
+++ erasmus/cogs/misc.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+from importlib import metadata
+
 import discord
-from botus_receptus import Cog, formatting
-from botus_receptus.app_commands import admin_guild_only
+from botus_receptus import Cog
+from botus_receptus.util import create_embed, send_context, send_interaction
 from discord import app_commands
 from discord.ext import commands
 
@@ -32,20 +34,72 @@ class InviteView(discord.ui.View):
         )
 
 
+class AboutView(InviteView):
+    def __init__(self, application_id: int, /, *, timeout: float | None = 180) -> None:
+        super().__init__(application_id, timeout=timeout)
+
+        self.add_item(
+            discord.ui.Button(
+                label='Official Support Server', url='https://discord.gg/ncZtNu5zgs'
+            )
+        )
+
+
+def get_about_embed(bot: Erasmus) -> discord.Embed:
+    total_members = 0
+    total_unique = len(bot.users)
+
+    text = 0
+    voice = 0
+    guilds = 0
+    for guild in bot.guilds:
+        guilds += 1
+
+        if guild.unavailable or guild.member_count is None:
+            continue
+
+        total_members += guild.member_count
+        for channel in guild.channels:
+            if isinstance(channel, discord.TextChannel):
+                text += 1
+            elif isinstance(channel, discord.VoiceChannel):
+                voice += 1
+
+    dpy_version = metadata.distribution('discord.py').version
+
+    return create_embed(
+        fields=[
+            {'name': 'Guilds', 'value': str(guilds)},
+            {
+                'name': 'Members',
+                'value': f'{total_members} total\n{total_unique} unique',
+            },
+            {
+                'name': 'Channels',
+                'value': f'{text + voice} total\n{text} text\n{voice} voice',
+            },
+        ],
+        footer={
+            'text': f'Made with discord.py v{dpy_version}',
+            'icon_url': 'http://i.imgur.com/5BFecvA.png',
+        },
+        timestamp=discord.utils.utcnow(),
+    )
+
+
 class Misc(Cog[Erasmus]):
     @commands.command(brief='Get the invite link for Erasmus')
     @commands.cooldown(rate=2, per=30.0, type=commands.BucketType.channel)
     async def invite(self, ctx: Context, /) -> None:
-        await ctx.send(view=InviteView(self.bot.application_id), reference=ctx.message)
+        await send_context(ctx, view=InviteView(self.bot.application_id))
 
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def stats(self, ctx: Context, /) -> None:
-        await ctx.send(
-            formatting.code_block(
-                f'''Servers: {len(self.bot.guilds)}
-Users: {len(self.bot.users)}'''
-            )
+    @commands.command(brief='Get info about Erasmus')
+    @commands.cooldown(rate=2, per=30.0, type=commands.BucketType.channel)
+    async def about(self, ctx: Context, /) -> None:
+        await send_context(
+            ctx,
+            embed=get_about_embed(self.bot),
+            view=AboutView(self.bot.application_id),
         )
 
 
@@ -61,14 +115,14 @@ class MiscAppCommands(Cog[Erasmus]):
         )
 
     @app_commands.command()
-    @admin_guild_only
-    async def stats(self, interaction: discord.Interaction, /) -> None:
-        '''Show stats about Erasmus'''
-        await interaction.response.send_message(
-            formatting.code_block(
-                f'''Servers: {len(self.bot.guilds)}
-Users: {len(self.bot.users)}'''
-            )
+    @app_commands.checks.cooldown(rate=2, per=30.0, key=lambda i: i.user.id)
+    async def about(self, interaction: discord.Interaction, /) -> None:
+        '''Get info about Erasmus'''
+
+        await send_interaction(
+            interaction,
+            embed=get_about_embed(self.bot),
+            view=AboutView(self.bot.application_id),
         )
 
 
