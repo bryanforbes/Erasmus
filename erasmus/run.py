5a80f6e8d0ce2c677707ea5b193f2d4ab0bc574a
Bryan Forbes <bryan@reigndropsfall.net>
2022-04-11 13:26:07 -0500

    Use `WatchedFileHandler` for logging

--- erasmus/run.py
+++ erasmus/run.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from logging.handlers import WatchedFileHandler
+
 import uvloop
 from botus_receptus import cli
 
@@ -8,5 +10,5 @@ from .erasmus import Erasmus
 
 def main() -> None:
     uvloop.install()
-    runner = cli(Erasmus, './config.toml')
+    runner = cli(Erasmus, './config.toml', handler_cls=WatchedFileHandler)
     runner()
