dae431db787086bf7d0a0279e582303019d643f4
Bryan Forbes <bryan@reigndropsfall.net>
2022-04-28 10:06:13 -0500

    Update botus_receptus

--- erasmus/db/base.py
+++ erasmus/db/base.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 from typing import Final
 
-from botus_receptus.sqlalchemy import sessionmaker
+from botus_receptus.sqlalchemy import async_sessionmaker
 from sqlalchemy.orm import registry
 
 _mapper_registry: Final = registry()
 
 mapped: Final = _mapper_registry.mapped
-Session: Final = sessionmaker(expire_on_commit=False)
+Session: Final = async_sessionmaker(expire_on_commit=False)
