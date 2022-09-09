656b7590d478530f8ca1d2c1c9261a591a99aa65
Bryan Forbes <bryan@reigndropsfall.net>
2022-09-09 14:50:11 -0500

    Rename the 1689

---
+++ alembic/versions/d20ba96e7d26_rename_1689.py
@@ -0,0 +1,39 @@
+"""Rename 1689
+
+Revision ID: d20ba96e7d26
+Revises: cdf53768de2d
+Create Date: 2022-09-09 14:46:04.418800
+
+"""
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'd20ba96e7d26'
+down_revision = 'cdf53768de2d'
+branch_labels = None
+depends_on = None
+
+
+confessions = sa.table(
+    'confessions',
+    sa.column('command', sa.String),
+    sa.column('name', sa.String),
+)
+
+
+def upgrade():
+    op.execute(
+        confessions.update()
+        .filter(confessions.c.command == '1689')
+        .values(name='The London Baptist Confession of Faith (1689)')
+    )
+
+
+def downgrade():
+    op.execute(
+        confessions.update()
+        .filter(confessions.c.command == '1689')
+        .values(name='The Second London Baptist Confession of Faith')
+    )
