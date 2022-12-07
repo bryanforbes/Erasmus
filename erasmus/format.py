12f2176813464c4c93b54febab7e7222f1fd696e
Bryan Forbes <bryan@reigndropsfall.net>
2022-12-07 15:53:17 -0600

    Bump deps

--- erasmus/format.py
+++ erasmus/format.py
@@ -6,6 +6,7 @@ _roman_pairs: Final = tuple(
     zip(
         ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'),
         (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1),
+        strict=True,
     )
 )
 
